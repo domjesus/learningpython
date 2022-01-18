4d8000f6a167304b8ce8d058c1c05b89672a4e7f
Wandeir Carneiro de Souza <domjesus@gmail.com>
2022-01-18 15:58:26 -0300

    Ajuste no layout e rotas

--- page_home/views.py
+++ page_home/views.py
@@ -1,5 +1,16 @@
+
 from http.client import HTTPResponse
+from django.template import RequestContext
 from django.shortcuts import render
 
+def ip_address_processor(request):
+    return {'ip_address': request.META['REMOTE_ADDR']}
+
 def index(request):
-    return render(request,'page_home/index.html')
+    # template = Template('{{ title }}: {{ ip_address }}')
+    context = RequestContext(request, {
+        'title': 'Your IP Address',
+    }, [ip_address_processor])
+    context2 = {"my_name":"Jose da Silva","ip":ip_address_processor(request)}
+    print(context)    
+    return render(request,'page_home/index.html', context2)
